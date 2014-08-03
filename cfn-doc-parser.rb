313afffe8a9c0a71263a9e80f26e4ac570ef34a2
Gildas Le Nadan <3ntr0p13+github@gmail.com>
2014-08-03 11:02:32 +0200

    made it a function that takes an url. Dl the page and creates the hash

--- cfn-doc-parser.rb
+++ cfn-doc-parser.rb
@@ -1,30 +1,42 @@
 #!/usr/bin/ruby -w
 
 require 'nokogiri'
+require 'open-uri'
 
-h = { :r => {}, :i => {}, :n => {} }
-file =  File.new('aws-properties-ec2-instance.html')
-doc = Nokogiri::HTML(file)
+def parse_page(url)
+  h = { :i => {}, :r => {}, :n => {} }
+  doc = Nokogiri::HTML(open(url))
+  # properties are in a list (dl) as dt for the key and dd for the content
+  properties = doc.search('//div[@class="variablelist"]/dl')
+  properties.search('dt').each do |dt|
+    property = dt.text
 
-# properties are in a list (dl) as dt for the key and dd for the content
-properties = doc.search('//div[@class="variablelist"]/dl')
-properties.search('dt').each do |dt|
-  property = dt.text
-
-  dd = dt.next_element
-  dd.search('p').each do |p|
-    if p.text.include?'Update requires'
-      effect = p.text.sub('Update requires:','')
-      if effect.include?'Replacement'
-        h[:r][property.to_sym] = effect
-      end
-      if effect.include?'Some interruptions'
-        h[:i][property.to_sym] = effect
-      end
-      if effect.include?'No interruption'
-        h[:n][property.to_sym] = effect
+    dd = dt.next_element
+    dd.search('p').each do |p|
+      if p.text.include?'Update requires'
+        effect = p.text.sub('Update requires:','')
+        if effect.include?'Replacement'
+          h[:r][property.to_sym] = effect
+        end
+        if effect.include?'Some interruptions'
+          h[:i][property.to_sym] = effect
+        end
+        if effect.include?'No interruption'
+          h[:n][property.to_sym] = effect
+        end
       end
     end
   end
+  h
 end
-p h
+
+aws_doc_root = 'http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/'
+aws_pages = ['aws-properties-cloudfront-distribution.html', 'aws-properties-ec2-instance.html']
+aws_pages = [
+  'aws-properties-ec2-instance.html',
+  'aws-properties-cloudfront-distribution.html'
+]
+
+aws_pages.each do |page|
+  p parse_page("#{aws_doc_root}#{page}")
+end
